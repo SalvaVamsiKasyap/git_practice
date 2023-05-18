2c28581ef06a8ff8ca7ad6c6828b7590491db8ee
vamsi <kasyapvamsi363@gmail.com>
2023-05-18 01:13:23 +0000

    Edited conftest.py

--- conftest.py
+++ conftest.py
@@ -17,6 +17,9 @@ from utilities.BaseClass import BaseClass
 import datetime
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options
+import requests
+import zipfile
+import os
 
 
 
@@ -39,12 +42,29 @@ def setup(request):
     browser_Name = request.config.getoption("browser_Name")
     response = requests.head("https://itero.com/en-APAC")
     response = str(response)[11:14]
+    chrome_version = '113.0.5672.126'  # Replace with your Chrome browser version
+    chrome_driver_url = f'https://chromedriver.storage.googleapis.com/{chrome_version}/chromedriver_linux64.zip'
+
+    # Download and extract the ChromeDriver file
+    response = requests.get(chrome_driver_url)
+    response.raise_for_status()
+
+    # Save the downloaded file
+    with open('chromedriver.zip', 'wb') as file:
+        file.write(response.content)
+
+    # Extract the contents of the ZIP file
+    with zipfile.ZipFile('chromedriver.zip', 'r') as zip_ref:
+        zip_ref.extractall()
+
+    # Set the executable path for ChromeDriver
+    chromedriver_path = os.path.abspath('chromedriver')
     try:
         if browser_Name == "chrome" and response == '200':
             log.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')} Successfully Chosen chrome browser")
             chrome_options = Options()
             chrome_options.binary_location = '/usr/bin/google-chrome'
-            driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
+            driver = webdriver.Chrome(chromedriver_path,options=chrome_options)
             log.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')} Successfully downloaded latest version of chrome driver {driver}")
         elif browser_Name == "firefox" and response == '200':
             firefox_options = Options()
